3a58b68e0e440fbb9043f0338437ddc3e49aa53e
Shabarish Ramaswamy <shabarishramaswamy@gmail.com>
2025-10-26 18:47:33 -0700

    Handled Control+C to gracefully shutdown taking down the DB connection

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/shabarishramaswamy/key-value-store/src/commonMethods"
 	"github.com/shabarishramaswamy/key-value-store/src/databases"
@@ -15,6 +18,9 @@ import (
 var PORT string = ":8529"
 
 func main() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
 	db, err := databases.RunInitDBLoop()
 	if err != nil {
 		return
@@ -24,5 +30,11 @@ func main() {
 	router := router.GetNewRouter(ctx, db)
 
 	fmt.Println("Listening to PORT ", PORT)
-	log.Fatal(http.ListenAndServe(PORT, router))
+	go func() {
+		log.Fatal(http.ListenAndServe(PORT, router))
+	}()
+	sig := <-quit
+
+	log.Printf("Main: Received signal: %v. Initiating graceful shutdown...", sig)
+	db.Close()
 }
